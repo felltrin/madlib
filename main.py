0a8e9ce09448c002966eac4873e8ec806e972f09
Lucius Holzinger <luciholzinger@gmail.com>
2023-07-21 12:58:30 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+
+def main():
+    root = tk.Tk()
+    root.geometry("300x300")
+
+
+
+
+    root.mainloop()
+    print("Hello World!")
+
+
+if __name__ == '__main__':
+    main()
