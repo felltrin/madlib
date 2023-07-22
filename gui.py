4232a8f37210c025c0d105e3c902705ac8ce1d76
Lucius Holzinger <luciholzinger@gmail.com>
2023-07-21 22:45:18 -0700

    refactored the code so it looks better and less lines overall

--- gui.py
+++ gui.py
@@ -9,9 +9,6 @@ class GUI:
         self.person_in_room = None
         self.noun_one = None
         self.noun_two = None
-        self.person_in_room_text = None
-        self.noun_one_text = None
-        self.noun_two_text = None
 
         self.root = tk.Tk()
         self.root.geometry("350x350")
@@ -20,11 +17,11 @@ class GUI:
         self.label_frame = tk.Frame(self.root)
         self.label_frame.columnconfigure(0, weight=1)
 
-        self.label_one = tk.Label(self.label_frame, text="Enter an adjective:", font=("Sans Serif", 16))
-        self.label_one.grid(row=0, column=0)
+        self.label = tk.Label(self.label_frame, text="Enter an adjective:", font=("Sans Serif", 16))
+        self.label.grid(row=0, column=0)
 
-        self.adjective_text = tk.Text(self.label_frame, height=1, width=15, font=("Sans Serif", 12))
-        self.adjective_text.grid(row=0, column=1)
+        self.text = tk.Text(self.label_frame, height=1, width=15, font=("Sans Serif", 12))
+        self.text.grid(row=0, column=1)
 
         self.label_frame.pack(padx=10, pady=10)
 
@@ -34,77 +31,41 @@ class GUI:
         self.root.mainloop()
 
     def submit_adjective(self):
-        self.adjective = self.adjective_text.get('1.0', tk.END)
+        self.adjective = self.text.get('1.0', tk.END)
         self.adjective = self.adjective.strip()
 
-        self.label_frame.destroy()
-        self.submit_button.destroy()
+        self.text.delete("1.0", "end")
 
-        self.label_frame = tk.Frame(self.root)
-        self.label_frame.columnconfigure(0, weight=1)
-
-        self.label_one = tk.Label(self.label_frame, text="Enter a person in the room:", font=("Sans Serif", 14))
-        self.label_one.grid(row=0, column=0)
-
-        self.person_in_room_text = tk.Text(self.label_frame, height=1, width=10, font=("Sans Serif", 12))
-        self.person_in_room_text.grid(row=0, column=1)
-
-        self.label_frame.pack(padx=10, pady=10)
-
-        self.submit_button = tk.Button(self.root, text="Submit", font=("Sans Serif", 12), command=self.submit_person_in_room)
-        self.submit_button.pack(padx=5, pady=5)
+        self.label["text"] = "Enter a person in the room:"
+        self.label["font"] = ("Sans Serif", 14)
+        self.submit_button.config(command=self.submit_person_in_room)
 
     def submit_person_in_room(self):
-        self.person_in_room = self.person_in_room_text.get('1.0', tk.END)
+        self.person_in_room = self.text.get('1.0', tk.END)
         self.person_in_room = self.person_in_room.strip()
 
-        self.label_frame.destroy()
-        self.submit_button.destroy()
+        self.text.delete('1.0', "end")
 
-        self.label_frame = tk.Frame(self.root)
-        self.label_frame.columnconfigure(0, weight=1)
-
-        self.label_one = tk.Label(self.label_frame, text="Enter a noun:", font=("Sans Serif", 16))
-        self.label_one.grid(row=0, column=0)
-
-        self.noun_one_text = tk.Text(self.label_frame, height=1, width=10, font=("Sans Serif", 12))
-        self.noun_one_text.grid(row=0, column=1)
-
-        self.label_frame.pack(padx=10, pady=10)
-
-        self.submit_button = tk.Button(self.root, text="Submit", font=("Sans Serif", 12), command=self.submit_noun_one)
-        self.submit_button.pack(padx=5, pady=5)
+        self.label["text"] = "Enter a noun:"
+        self.label["font"] = ("Sans Serif", 16)
+        self.submit_button.config(command=self.submit_noun_one)
 
     def submit_noun_one(self):
-        self.noun_one = self.noun_one_text.get('1.0', tk.END)
+        self.noun_one = self.text.get('1.0', tk.END)
         self.noun_one = self.noun_one.strip()
 
-        self.label_frame.destroy()
-        self.submit_button.destroy()
+        self.text.delete('1.0', "end")
 
-        self.label_frame = tk.Frame(self.root)
-        self.label_frame.columnconfigure(0, weight=1)
-
-        self.label_one = tk.Label(self.label_frame, text="Enter another noun:", font=("Sans Serif", 16))
-        self.label_one.grid(row=0, column=0)
-
-        self.noun_two_text = tk.Text(self.label_frame, height=1, width=10, font=("Sans Serif", 12))
-        self.noun_two_text.grid(row=0, column=1)
-
-        self.label_frame.pack(padx=10, pady=10)
-
-        self.submit_button = tk.Button(self.root, text="Submit", font=("Sans Serif", 12),
-                                       command=self.submit_final_entry)
-        self.submit_button.pack(padx=5, pady=5)
+        self.label["text"] = "Enter another noun:"
+        self.label["font"] = ("Sans Serif", 16)
+        self.submit_button.config(command=self.submit_final_entry)
 
     def submit_final_entry(self):
-        self.noun_two = self.noun_two_text.get('1.0', tk.END)
+        self.noun_two = self.text.get('1.0', tk.END)
         self.noun_two = self.noun_two.strip()
 
         self.label_frame.destroy()
         self.submit_button.destroy()
-
-        # self.root.geometry("500x500")
 
         madlib_text = "This Thanksgiving, I'm thankful for all the " + self.adjective + \
                       " things in my life. Even though I complain about how " + self.person_in_room + \
